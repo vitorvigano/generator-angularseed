caac379660537e4c431547576953a20662668f0a
vitorvigano <vitorvigano@gmail.com>
2013-07-04 19:15:56 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Generator-angularjs
+[![Build Status](https://secure.travis-ci.org/vitorvigano/generator-angularjs.png?branch=master)](https://travis-ci.org/vitorvigano/generator-angularjs)
+
+A generator for Yeoman.
+
+## Getting started
+- Make sure you have [yo](https://github.com/yeoman/yo) installed:
+    `npm install -g yo`
+- Install the generator: `npm install -g generator-angularjs`
+- Run: `yo angularjs`
+
+## License
+[MIT License](http://en.wikipedia.org/wiki/MIT_License)
---
+++ app/index.js
@@ -0,0 +1,73 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+
+var AngularjsGenerator = module.exports = function AngularjsGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(AngularjsGenerator, yeoman.generators.Base);
+
+AngularjsGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // have Yeoman greet the user.
+  console.log(this.yeoman);
+
+  var prompts = [{
+    type: 'confirm',
+    name: 'someOption',
+    message: 'Would you like to enable this option?',
+    default: true
+  }];
+
+  this.prompt(prompts, function (props) {
+    this.someOption = props.someOption;
+
+    cb();
+  }.bind(this));
+};
+
+AngularjsGenerator.prototype.app = function app() {
+  this.mkdir('app');
+  this.mkdir('app/img');
+  this.mkdir('app/css');
+  this.mkdir('app/js');
+  this.mkdir('app/lib');
+  this.mkdir('app/lib/angular');
+  this.mkdir('app/views');
+    
+    // grunt
+    this.template('Gruntfile.js', 'Gruntfile.js');
+  this.template('_package.json', 'package.json');
+    // bower
+  this.template('_bower.json', 'bower.json');
+    // index.html
+  this.template('index.html', 'app/index.html');
+    // css files
+  this.template('app.css', 'app/css/app.css');
+    // js files
+  this.template('app.js', 'app/js/app.js');  
+  this.template('services.js', 'app/js/services.js');
+  this.template('controllers.js', 'app/js/controllers.js');
+  this.template('filters.js', 'app/js/filters.js');
+  this.template('directives.js', 'app/js/directives.js');
+};
+
+AngularjsGenerator.prototype.projectfiles = function projectfiles() {
+  this.copy('editorconfig', '.editorconfig');
+  this.copy('jshintrc', '.jshintrc');
+};
+
+AngularjsGenerator.prototype.runtime = function app() {
+ 
+  this.copy('gitignore', '.gitignore');
+};
