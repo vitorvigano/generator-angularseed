d999246a7ea6209b5533d4e7447c82359d19de94
Vitor Vigano <vitorvigano@gmail.com>
2013-08-27 21:35:47 -0300

    test modified

--- test/test-creation.js
+++ test/test-creation.js
@@ -22,8 +22,8 @@ describe('angularseed generator', function () {
   it('creates expected files', function (done) {
     var expected = [
       // add files you expect to exist here.
-      '.jshintrc',
-      '.editorconfig'
+      '.aws-config.json',
+      '.gitignore'
     ];
 
     helpers.mockPrompt(this.app, {
